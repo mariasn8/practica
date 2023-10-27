f6b2040e1cc928e71eeed8f0b810087cd4ba13af
mariasn8 <maria.sancheznieto@usp.ceu.es>
2023-10-27 14:02:21 +0200

    oo

--- src/dominio/Pelicula.java
+++ src/dominio/Pelicula.java
@@ -8,9 +8,26 @@ package dominio;
 
 public class Pelicula{
 
-	String titulo;
-	String director;
-	int annoEstreno;
+	private String titulo;
+	private String director;
+	private int annoEstreno;
+	private int id;
+
+	/**
+	 * Constructor con parámetros
+	 * @param id id de la película
+	 * @param titulo título de la película
+	 * @param director director de la película
+	 * @param annoEstreno año de estreno de la película
+	 *
+	 */
+
+	public Pelicula(int id, String titulo, String director, int annoEstreno){
+		this.id=id;
+		this.titulo=titulo;
+		this.director=director;
+		this.annoEstreno=annoEstreno;
+	}
 
 	/**
 	 * Constructor con parámetros
@@ -24,6 +41,15 @@ public class Pelicula{
 		this.titulo=titulo;
 		this.director=director;
 		this.annoEstreno=annoEstreno;
+	}
+
+	/**
+	 * Constructor con parámetros
+	 * @param id id de la película
+	 */
+
+	public Pelicula(int id){
+		this.id=id;
 	}
 
 	/**
@@ -56,13 +82,44 @@ public class Pelicula{
 	}
 
 	/**
+	 * Devuelve el id de la película
+	 * @return id de la película
+	 */
+
+	public int getId(){
+		return id;
+	}
+
+	/**
+	 * Asigna un id a una película
+	 * @param id id de la película
+	 */
+
+	public void setId(int id){
+		this.id=id;
+	}
+
+	/**
+	 * Comprueba que el id introducido coincida con el de la película
+	 * @return id de la película
+	 *
+	 */
+
+	@Override
+	public boolean equals(Object obj){
+		Pelicula peli=(Pelicula) obj;
+		return id==peli.id;
+	}
+
+	/**
 	 * Devuelve una cadena de caracteres con el título, director y año de estreno de la película
 	 * @return cadena de caracteres con la información de la película
 	 *
 	 */
 
+	@Override
 	public String toString(){
-		return "El título de la película es '"+getTitulo()+"', '"+getDirector()+"' es su director y "+getAnnoEstreno()+" es su año de estreno";
+		return "Id: "+getId()+". Su título es: '"+getTitulo()+"', su director/a es: '"+getDirector()+"' y su año de estreno es: "+getAnnoEstreno();
 	}
 }
 
--- src/interfaz/Interfaz.java
+++ src/interfaz/Interfaz.java
@@ -36,6 +36,10 @@ public class Interfaz{
 			Pelicula peli = new Pelicula(instruccion[1], instruccion[2], Integer.parseInt(instruccion[3]));
 			cat.addPeli(peli);
 		}
+		else if(instruccion[0].equalsIgnoreCase("borrar") && instruccion.length == 2){
+			Pelicula peli = new Pelicula(Integer.parseInt(instruccion[1]));
+			cat.borrarPeli(peli);
+		}
 		else if (instruccion[0].equalsIgnoreCase("ayuda") && instruccion.length == 1){
 			ayuda();
 		}
@@ -57,21 +61,28 @@ public class Interfaz{
 		System.out.println("\n"+negrita+"DESCRIPCIÓN"+normal);
 		System.out.println("\tEsta aplicación ofrece las siguientes funcionalidades:\n\n" + 
 				"\t- Permite añadir una nueva película al catálogo\n" + 
-				"\t- Permite mostrar las películas del catálogo");
+				"\t- Permite mostrar las películas del catálogo\n" +
+				"\t- Permite borrar alguna de las películas añadidas al catálogo\n");
+
 		System.out.println(negrita+"FORMATO"+normal);
 		System.out.println("\tPara añadir una nueva película, se escribe:\n");
-		System.out.println("\t\t" + negrita+ "java -cp bin aplicacion.Principal anadir <'titulo'> <'director'> <año de estreno>"+normal+"\n");
+		System.out.println("\t\t" + negrita+ "java -jar catalogoPelis.jar anadir <'titulo'> <'director'> <año de estreno>"+normal+"\n");
 		System.out.println("\tPara mostrar las películas del catálogo, se teclea:\n");
-		System.out.println("\t\t" + negrita + "java -cp bin aplicacion.Principal mostrar" + normal + "\n");
+		System.out.println("\t\t" + negrita + "java -jar catalogoPelis.jar mostrar" + normal + "\n");
 		System.out.println("\tPara mostrar esta ayuda, se escribe:\n");
-		System.out.println("\t\t" + negrita +"java -cp bin aplicacion.Principal ayuda" + normal + "\n");
+		System.out.println("\t\t" + negrita +"java -jar catalogoPelis.jar ayuda" + normal + "\n");
+		System.out.println("\tPara borrar una de las películas del catálogo introduzca:\n");
+		System.out.println("\t\t" + negrita +"java -jar catalogoPelis.jar borrar <id de la película>" + normal + "\n");
+
 		System.out.println(negrita+"EJEMPLOS"+normal);
 		System.out.println("\tEjemplo 1\n");
-		System.out.println("\t\t" + negrita +"java -cp bin aplicacion.Principal anadir 'Matrix' 'Lana Wachowsky' 1999" + normal + "\n");
+		System.out.println("\t\t" + negrita +"java -jar catalogoPelis.jar anadir 'Matrix' 'Lana Wachowsky' 1999" + normal + "\n");
 		System.out.println("\tEjemplo 2\n");
-		System.out.println("\t\t" + negrita + "java -cp bin aplicacion.Principal mostrar" + normal+ "\n");
+		System.out.println("\t\t" + negrita + "java -jar catalogoPelis.jar mostrar" + normal+ "\n");
 		System.out.println("\tEjemplo 3\n");
-		System.out.println("\t\t" + negrita + "java -cp bin aplicacion.Principal ayuda" + normal + "\n");
+		System.out.println("\t\t" + negrita + "java -jar catalogoPelis.jar ayuda" + normal + "\n");
+		System.out.println("\tEjemplo 4\n");
+		System.out.println("\t\t" + negrita + "java -jar catalogoPelis.jar borrar 2" + normal + "\n");
 	}
 
 }
