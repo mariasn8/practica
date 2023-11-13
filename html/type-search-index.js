2cf657fb6d523298e99c35efd6fe4b210cfeaad8
mariasn8 <maria.sancheznieto@usp.ceu.es>
2023-11-13 12:42:35 +0100

    qq

---
+++ html/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"dominio","l":"Catalogo"},{"p":"interfaz","l":"Interfaz"},{"p":"dominio","l":"Pelicula"},{"p":"aplicacion","l":"Principal"}]
